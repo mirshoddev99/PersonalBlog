8493e09deeccde2c31370f2ca26a880568516643
mirshod <106139041+mirshoddev99@users.noreply.github.com>
2022-10-04 22:53:24 +0900

    detected some issues and fixed

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from environs import Env
-env = Env()
-env.read_env()
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = 'django-insecure-6zh#1m@4f)3ngqb6)3tdbe-up*4ko52d9w)(cwsmuxlalfxz8+'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = False
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
