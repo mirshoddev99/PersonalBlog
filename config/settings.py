3ef59192b2ef408816768430dd876775bbcc16f1
mirshod <oripovmirshod9@gmail.com>
2023-02-07 17:38:49 +0900

    all issues was solved

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6zh#1m@4f)3ngqb6)3tdbe-up*4ko52d9w)(cwsmuxlalfxz8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Deployment settings
 SESSION_COOKIE_SECURE = True
